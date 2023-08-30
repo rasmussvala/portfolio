9dfd22d2b67784a7b90274b43cbdd2bf954bb4dd
rasmussvala <rassv453@student.liu.se>
2023-08-30 20:08:59 +0200

    lagt till halvt fungerande profilbild

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import "./styles/card.css";
+import profileImage from "./assets/Memoji.png";
 
 function App() {
   return (
     <>
+      <header className="header">
+        <div className="text-container">
+          <h1>Rasmus Svala</h1>
+          <p>Hej! </p>
+        </div>
+        <div className="image-container">
+          <div className="profile-image">
+            <img src={profileImage} alt="Din bild" />
+          </div>
+        </div>
+      </header>
+
       <section className="card-list">
         <article className="card">
           <header className="card-header">
@@ -16,6 +29,10 @@ function App() {
             <p>21 juni 2023</p>
             <h2>Detta är en text som är i h2</h2>
           </header>
+          <div className="card-image-container">
+            <img src={require("./assets/load.gif")} alt="temp" />
+          </div>
+          <p>Detta är brödtext som är i p</p>
         </article>
 
         <article className="card">
