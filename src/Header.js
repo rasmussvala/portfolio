ffa1f4a5c3abb9d4080f4168c472fb2bbc242280
rasmussvala <rassv453@student.liu.se>
2024-02-02 15:05:34 +0100

    added social links

--- src/Header.js
+++ src/Header.js
@@ -1,26 +1,32 @@
 import React from "react";
 import profileImage from "./assets/profile-pic.png";
+import Links from "./Links";
 
 export default function Header() {
   return (
-    <header className="header">
-      <div className="text-container">
-        <h1>Rasmus Svala</h1>
-        <p>
-          Hey there! 👋 I'm Rasmus, a 4th year Master of Science student in
-          Media Technology and Engineering, based in Sweden. I've got a passion
-          for computer graphics and web development. When I'm not diving into
-          pixels and code, you might just find me at 'Pub Vattentornet',
-          appreciating a good beer and catching up with friends. 🍻 My aim here
-          is to keep things simple and showcase a glimpse of some of the work
-          I've done. Feel free to contact me anytime via my social networks. 😊{" "}
-        </p>
-      </div>
-      <div className="image-container">
-        <div className="profile-image">
-          <img src={profileImage} alt="Din bild" />
+    <>
+      <header className="header">
+        <div className="text-container">
+          <h1>Rasmus Svala</h1>
+          <p>
+            Hey there! 👋 I'm Rasmus, a 4th year Master of Science student in
+            Media Technology and Engineering, based in Sweden. I've got a
+            passion for computer graphics and web development. When I'm not
+            diving into pixels and code, you might just find me at 'Pub
+            Vattentornet', appreciating a good beer and catching up with
+            friends. 🍻 My aim here is to keep things simple and showcase a
+            glimpse of some of the work I've done. Feel free to contact me
+            anytime via my social networks. 😊{" "}
+          </p>
+          <br></br>
+          <Links />
         </div>
-      </div>
-    </header>
+        <div className="image-container">
+          <div className="profile-image">
+            <img src={profileImage} alt="Din bild" />
+          </div>
+        </div>
+      </header>
+    </>
   );
 }
