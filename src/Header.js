b7f47afc5c075dc724a1b9dfe220885bf501786a
rasmussvala <rassv453@student.liu.se>
2024-05-31 17:50:55 +0200

    new font and color

--- src/Header.js
+++ src/Header.js
@@ -9,14 +9,10 @@ export default function Header() {
         <div className="text-container">
           <h1>Rasmus Svala</h1>
           <p>
-            Hey there! 👋 I'm Rasmus, a 4th year Master of Science student in
-            Media Technology and Engineering, based in Sweden. I've got a
-            passion for computer graphics and web development and when I'm not
-            coding, you might just find me at 'Pub Vattentornet', appreciating a
-            good beer and catching up with friends. 🍻 My aim here is to keep
-            things simple and showcase a glimpse of some of the work I've done
-            alone or with friends. Feel free to contact me anytime via my social
-            networks. 😊{" "}
+            Hi. My name is Rasmus and I am a fourt year MSc student in Media
+            Technology and Engineering, based in Sweden. My aim here is to keep
+            things simple and showcase some of the work I've done alone or in
+            group. Feel free to contact me anytime via my social networks. 😊{" "}
           </p>
           <br></br>
           <Links />
