08d0cdb0a230a6ecd8981c42040e1905f2700421
rasmussvala <rassv453@student.liu.se>
2024-06-06 13:34:47 +0200

    fixed large desktop screens
    now everything is centered

--- src/App.js
+++ src/App.js
@@ -30,31 +30,33 @@ function App() {
       <Header />
       <Links />
 
-      <h2>About me</h2>
-      <div className="about-me-text">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-      </div>
+      <div className="content-container">
+        <h2>About me</h2>
+        <div className="about-me-text">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
 
-      <h2>Projects</h2>
-      <section className="card-list">
-        {sortedProjects.map((project, index) => (
-          <Card
-            key={index}
-            date={project.date}
-            title={project.title}
-            image={project.image}
-            description={project.description}
-          />
-        ))}
-      </section>
+        <h2>Projects</h2>
+        <section className="card-list">
+          {sortedProjects.map((project, index) => (
+            <Card
+              key={index}
+              date={project.date}
+              title={project.title}
+              image={project.image}
+              description={project.description}
+            />
+          ))}
+        </section>
+      </div>
     </div>
   );
 }
