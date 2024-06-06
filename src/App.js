52db3f73896748539a43c0286a3d21808bc038a7
rasmussvala <rassv453@student.liu.se>
2024-06-06 13:09:55 +0200

    added about me, no text

--- src/App.js
+++ src/App.js
@@ -30,6 +30,19 @@ function App() {
       <Header />
       <Links />
 
+      <h2>About me</h2>
+      <div className="about-me-text">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+
       <h2>Projects</h2>
       <section className="card-list">
         {sortedProjects.map((project, index) => (
